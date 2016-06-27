efad2bdeee618d2d53c093287aa42f4f13dbe566
The Mac Admin <miles@themacadmin.com>
2016-06-27 13:12:51 -0700

    Working outline
    Require password for all sudo commands

---
+++ removeSudoTimeout.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# remove sudo timeout
+
+# Make a backup of sudoers file
+cp /etc/sudoers /etc/sudoers.bak
+
+# Verify backup sudoers file
+visudo -c -f /etc/sudoers.bak
+## add bailout and error message if failed
+
+# Make a working copy of sudoers file
+cp /etc/sudoers /etc/sudoers.work
+
+# Verify working copy of sudoers file
+visudo -c -f /etc/sudoers.work
+## add bailout and error message if failed
+
+# Modify timeout setting in working copy of sudoers file
+chmod u+w /etc/sudoers.work
+echo "Defaults timestamp_timeout=0" >> /etc/sudoers.work
+chmod u-w /etc/sudoers.work
+
+# Verify working copy of sudoers file
+visudo -c -f /etc/sudoers.work
+## add bailout and error message if failed
+
+# Replace sudoers file with working copy
+mv /etc/sudoers.work /etc/sudoers
